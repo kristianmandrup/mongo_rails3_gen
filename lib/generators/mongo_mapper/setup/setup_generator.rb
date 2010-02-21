6ee0242e52caafa321a7d3f5657012b4f65bda14
Kristian Mandrup <kmandrup@gmail.com>
2010-02-21 16:45:10 -0500

    seems to work

--- lib/generators/mongo_mapper/setup/setup_generator.rb
+++ lib/generators/mongo_mapper/setup/setup_generator.rb
@@ -1,12 +1,22 @@
-require 'thor-ext'
+# require 'thor-ext'
 require 'rails/generators/base'
 
 module MongoMapper
   module Generators
     class SetupGenerator < Rails::Generators::Base            
-      include ThorExtensions
+      # include ThorExtensions
 
       argument :database, :type => :string, :default => 'mongo_db_default'            
+
+      # def configure_gems
+      #   cleanup_gemfile
+      #   add_gems %w{mongo_ext mongo mongo_mapper}
+      # end
+
+      def create_files
+        template "mongo_mapper_db_config.rb" , "config/initializers/mongo_mapper_db_config.rb"
+        puts "Please ensure Gemfile contains: gem 'mongo_mapper'"
+      end
 
       def self.source_root
         @source_root ||= File.expand_path('../templates', __FILE__)
@@ -16,14 +26,6 @@ module MongoMapper
         "#{$0} mongo_mapper:#{generator_name} #{self.arguments.map{ |a| a.usage }.join(' ')} [options]"
       end
 
-      def configure_gems
-        cleanup_gemfile
-        add_gems %w{mongo_ext mongo mongo_mapper}
-      end
-
-      def create_files
-        template "mongo_mapper_db_config.rb" , "config/initializers/mongo_mapper_db_config.rb" 
-      end
       
     end
   end
--- lib/generators/mongoid/setup/setup_generator.rb
+++ lib/generators/mongoid/setup/setup_generator.rb
@@ -1,12 +1,24 @@
-require 'thor-ext'
+# require 'thor-ext'
 require 'rails/generators/base'
 
 module Mongoid
   module Generators
     class SetupGenerator < Rails::Generators::Base            
-      include ThorExtensions
+      # include ThorExtensions
 
       argument :database, :type => :string, :default => 'mongo_db_default'            
+
+      # def configure_gems
+      #   cleanup_gemfile
+      #   add_gems %w{mongo_ext mongo mongoid}
+      # end
+
+      def create_files
+        template "mongoid.rb" , "config/initializers/mongoid.rb" 
+        template 'database.mongo.yml', "config/database.mongo.yml"   
+
+        puts "Please ensure Gemfile contains: gem 'mongoid'"
+      end
 
       def self.source_root
         @source_root ||= File.expand_path('../templates', __FILE__)
@@ -14,16 +26,6 @@ module Mongoid
 
       def self.banner
         "#{$0} mongoid:#{generator_name} #{self.arguments.map{ |a| a.usage }.join(' ')} [options]"
-      end
-
-      def configure_gems
-        cleanup_gemfile
-        add_gems %w{mongo_ext mongo mongoid}
-      end
-
-      def create_files
-        template "mongoid.rb" , "config/initializers/mongoid.rb" 
-        template 'database.mongo.yml', "config/database.mongo.yml"
       end
 
     end
